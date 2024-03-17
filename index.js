20f53afc9482ae3421b5df38509b2050e0e09837
appleberry18 <86164381+ap-berry@users.noreply.github.com>
2024-03-17 16:23:20 +0600

    I got it working. Added functionality for chaning values like c.a, c.b, maxIteration and limit of convergence (lim)

--- index.js
+++ index.js
@@ -1,47 +1,74 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.getElementById("mycanvas").getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    const canvas = document.getElementById("mycanvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 1000//window.innerWidth;
+    canvas.height = 1000 //window.innerHeight;
     const width = canvas.width
     const height = canvas.height
-    canvas.translate(width/2, height/2);
+    ctx.translate(width/2, height/2);
 
-    setInterval(() => {
+    setInterval(async () => {
         document.getElementById("clock").innerText = new Date().toTimeString()
     }, 1E3);
-    const m = 255/50;
-
-    const c = { a: 0.1, b : 0.3 };
-
-    document.getElementById("start").addEventListener("click", () => {
+    const slider_a = document.getElementById("a")
+    slider_a.oninput = () => {
+        document.getElementById("a_label").innerText = `c.a = ${slider_a.value/100}`;
+    }
+    const slider_b = document.getElementById("b")
+    slider_b.oninput = () => {
+        document.getElementById("b_label").innerText = `c.b = ${slider_b.value/100}`;
+    }
+    const slider_lim = document.getElementById("lim")
+    slider_lim.oninput = () => {
+        document.getElementById("lim_label").innerText = `limit = ${slider_lim.value}`;
+    }
+    const slider_maxIter = document.getElementById("maxIter")
+    slider_maxIter.oninput = () => {
+        document.getElementById("maxIter_label").innerText = `maximum Iteration = ${slider_maxIter.value}`;
+    }
+    document.getElementById("start").addEventListener("click", start)
+    
+    
+    async function start(){
+        const c = { a: slider_a.value/100, b : slider_b.value/100 };
+        const lim = slider_lim.value
+        const maxiter = slider_maxIter.value
+        const m = 255/maxiter;
         for(let x = -width/2; x <= width/2; x++){
-            for(let y = height/2; y <= height/2; y++){
-                let rgb = Math.floor(m*count_itteration(x, y, c, 50))
-                canvas.fillStyle = `red`;
-                canvas.fillRect(x, y, 1, 1);
+            for(let y = -height/2; y <= height/2; y++){
+                let rgb = Math.floor(m*count_itteration((x*2*lim/width), (y*2*lim/width), c, maxiter, lim))
+                ctx.fillStyle = `${rgbToHex(rgb, rgb, rgb)}`;
+                ctx.fillRect(x, y, 1, 1);
+                
             }
         }
-    })
+    }
 })  
 
 function compute_next(z, c){
     let z2 = {
         "x" : (z.x*z.x - z.y*z.y + c.a),
-        "y" : (2*z.x*z.x + c.b) 
+        "y" : (2*z.x*z.y + c.b) 
     }
     return z2;
 }
 
-function count_itteration(x, y, c, maxIteration){
+function mod(z){
+    return z.x*z.x + z.y*z.y
+}
+function count_itteration(x, y, c, maxIteration, lim){
     let z = { 
         "x" : x, 
         "y" : y
     }
 
     let i = 0
+    lim = Math.pow(lim, 2)
     while(i < maxIteration){
         let z2 = compute_next(z, c);
-        if(z2.x > 2) break;
+        if(mod(z2) > lim) break;
+        z.x = z2.x 
+        z.y = z2.y
         i++;
     }
     return i;
